5c56c481a69950422696823cbfeea93436a2b422
peticol2026 <peticol2026@gmail.com>
2026-02-05 11:20:49 -0500

    Logica para busqueda barra de cards administrador

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,11 @@
 const toggle = document.getElementById("toggleSidebar");
 const sidebar = document.getElementById("sidebar");
 const content = document.querySelector(".content");
+const toggleBtn = document.getElementById("toggleSidebar");
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+
+
 
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("hidden");
@@ -12,5 +17,39 @@ document.querySelectorAll(".card").forEach(card => {
   card.addEventListener("click", () => {
     const link = card.dataset.link;
     window.location.href = link;
+  });
+});
+
+
+
+// ==============================
+// SIDEBAR TOGGLE
+// ==============================
+
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+});
+
+
+// ==============================
+// BUSCADOR DE CARDS
+// ==============================
+
+
+searchInput.addEventListener("input", () => {
+  const searchValue = searchInput.value.toLowerCase().trim();
+
+  cards.forEach(card => {
+    const cardText = card
+      .querySelector(".card-text")
+      .textContent
+      .toLowerCase();
+
+    if (cardText.includes(searchValue)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
   });
 });
